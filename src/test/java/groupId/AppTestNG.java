e4ee015a6629af58e9f948ea46214ae3da837baf
alexproj <43067368+alexproj@users.noreply.github.com>
2020-06-23 00:46:48 +0300

    reworked with testNG + dataprovider

--- src/test/java/groupId/AppTestNG.java
+++ src/test/java/groupId/AppTestNG.java
@@ -1,41 +1,34 @@
 package groupId;
 
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+public class AppTestNG {
 
-public class AppTest {
-
-    @Test
-    public void browserLaunch() {
+    @Test(dataProvider = "rozprovider")
+    public void browserLaunch(String itemToSearch) {
         System.setProperty("webdriver.chrome.driver", ".\\files\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://rozetka.com.ua/");
-        driver.findElement(By.name("search")).sendKeys("airpods");
+        driver.findElement(By.name("search")).sendKeys(itemToSearch);
         driver.findElement(By.cssSelector("form.search-form button.search-form__submit")).click();
-        List<WebElement> searchResults = driver.findElements(By.xpath("//span[contains(.,'Airpods') or contains(.,'AirPods')] "));
+        List<WebElement> searchResults = driver.findElements(By.xpath("//span[contains(.,'"+itemToSearch+"')]"));
         if (searchResults.size() != 0) {
-
             driver.findElement(By.className("toOrder")).click();
-            /*System.out.println(searchResults.size());
-            WebElement elem1= ((WebElement)searchResults.get(1));
-            System.out.println(elem1);
-            System.out.println(elem1.findElement(By.className("buy-button")).getTagName());*/
         }
 
     }
-    //System.out.println(elements.size());
-
-
+    @DataProvider(name = "rozprovider")
+    public Object[] searchItems(){
+        return new Object[] {"Airpods", "Bicycle"};
+    }
 }
-
-
-
-
